12f5082043352a410263e07d06210cc23a5701cb
lzy77_ <channelforletsplayshik@gmail.com>
2022-01-24 16:07:08 +0300

    Add implementation for edit command.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -64,6 +64,84 @@
             return record.Id;
         }
 
+        public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short height, decimal weight, char temperament)
+        {
+            int index = this.IsExistRecord(id);
+
+            if (index == -1)
+            {
+                throw new ArgumentException("Wrong id", $"{nameof(id)}");
+            }
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException($"{nameof(firstName)}", "Parameter is null");
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException("Parameter has wrong length", $"{nameof(firstName)}");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException($"{nameof(lastName)}", "Parameter is null");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException("Parameter has wrong length", $"{nameof(lastName)}");
+            }
+
+            DateTime min = new DateTime(1950, 1, 1);
+            if (DateTime.Compare(dateOfBirth, min) < 0 || DateTime.Compare(dateOfBirth, DateTime.Now) > 0)
+            {
+                throw new ArgumentException("Parameter is wrong", $"{nameof(dateOfBirth)}");
+            }
+
+            if (height < 45 || height > 252)
+            {
+                throw new ArgumentException("Parameter is wrong", $"{nameof(height)}");
+            }
+
+            if (weight < 2 || weight > 600)
+            {
+                throw new ArgumentException("Parameter is wrong", $"{nameof(weight)}");
+            }
+
+            temperament = char.ToUpper(temperament, new System.Globalization.CultureInfo("en-US"));
+            if (!(temperament == 'P' || temperament == 'S' || temperament == 'C' || temperament == 'M'))
+            {
+                throw new ArgumentException("Parameter is wrong", $"{nameof(temperament)}");
+            }
+
+            var record = new FileCabinetRecord
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth,
+                Height = height,
+                Weigth = weight,
+                Temperament = temperament,
+            };
+
+            this.list[index] = record;
+        }
+
+        public int IsExistRecord(int id)
+        {
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                if (this.list[i].Id == id)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
         public FileCabinetRecord[] GetRecords()
         {
             return this.list.ToArray();

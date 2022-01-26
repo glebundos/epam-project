e1c98e659e93bd77b0e784dc12008cbe105f40a9
lzy77_ <channelforletsplayshik@gmail.com>
2022-01-27 01:25:27 +0300

    Extract IFileCabinetService.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -5,7 +5,7 @@ namespace FileCabinetApp
     /// <summary>
     /// Represents standart operations with list of records.
     /// </summary>
-    public class FileCabinetService
+    public class FileCabinetService : IFileCabinetService
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
         private readonly IRecordValidator validator;
---
+++ FileCabinetApp/IFileCabinetService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Represents interface for services with standart operations with list of records.
+    /// </summary>
+    public interface IFileCabinetService
+    {
+        /// <summary>
+        /// Creates new record with given parameters.
+        /// </summary>
+        /// <param name="newRecord">Object with all record parameters.</param>
+        /// <returns>Id of the created record or -1 in case of error.</returns>
+        public int CreateRecord(Record newRecord);
+
+        /// <summary>
+        /// Edits the record with the given id according to the given parameters.
+        /// </summary>
+        /// <param name="id">The Id of the record to change.</param>
+        /// <param name="newRecord">Object with all record parameters.</param>
+        public void EditRecord(int id, Record newRecord);
+
+        /// <summary>
+        /// Searches for a record with given Id.
+        /// </summary>
+        /// <param name="id">The Id of the record to search.</param>
+        /// <returns>Index of the record in list, or -1 if record with that Id was not found.</returns>
+        public int RecordIndex(int id);
+
+        /// <summary>
+        /// Searches for all records with given first name (Ignoring lower case and upper case differences).
+        /// </summary>
+        /// <param name="firstName">The first name to search for.</param>
+        /// <returns>Array of records with given first name or an empty array if there are no records with that first name.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName);
+
+        /// <summary>
+        /// Searches for all records with given last name (Ignoring lower case and upper case differences).
+        /// </summary>
+        /// <param name="lastName">The last name to search for.</param>
+        /// <returns>Array of records with given last name or an empty array if there are no records with that last name.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName);
+
+        /// <summary>
+        /// Searches for all records with given date of birth.
+        /// </summary>
+        /// <param name="dateOfBirth">The date of birth to search for.</param>
+        /// <returns>Array of records with given date of birth or an empty array if there are no records with that date of birth.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByDateOfBirth(DateTime dateOfBirth);
+
+        /// <summary>
+        /// Gets the all records.
+        /// </summary>
+        /// <returns>Array of all records.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords();
+
+        /// <summary>
+        /// Gets the number of all records.
+        /// </summary>
+        /// <returns>Number of all records.</returns>
+        public int GetStat();
+    }
+}
